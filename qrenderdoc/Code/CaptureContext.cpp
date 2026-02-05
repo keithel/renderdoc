82bd0be12449b9cb980c272666d5c7cf2362d4d9
Keith Kyzivat <keith.kyzivat@qt.io>
2026-02-04 20:54:20 -0500

    Qt6 port: nullptr QPointer removal from List fix
    This replaces how nullptr QPointers are removed from a QList - in the
    newer Qt 6 way.

--- qrenderdoc/Code/CaptureContext.cpp
+++ qrenderdoc/Code/CaptureContext.cpp
@@ -489,7 +489,7 @@ rdcstr CaptureContext::LoadExtension(rdcstr name)
   for(QAction *a : m_MainWindow->GetMenuActions())
     CleanMenu(a);
 
-  m_RegisteredMenuItems.removeAll(nullptr);
+  m_RegisteredMenuItems.removeIf([](const auto &item) { return item.isNull(); });
 
   return ret;
 }
