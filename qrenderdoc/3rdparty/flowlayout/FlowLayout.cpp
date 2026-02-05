1905689e818912b6cdb3c49fb760f14152ef0034
Keith Kyzivat <keith.kyzivat@qt.io>
2026-02-04 22:31:52 -0500

    Qt 6 port: Pull in Qt 6 FlowLayout example changes
    FlowLayout is an example that still ships with Qt. Pull in the Qt 6
    changes from the example code into the renderdoc version.

--- qrenderdoc/3rdparty/flowlayout/FlowLayout.cpp
+++ qrenderdoc/3rdparty/flowlayout/FlowLayout.cpp
@@ -109,13 +109,12 @@ QLayoutItem *FlowLayout::takeAt(int index)
 {
     if (index >= 0 && index < itemList.size())
         return itemList.takeAt(index);
-    else
-        return 0;
+    return nullptr;
 }
 
 Qt::Orientations FlowLayout::expandingDirections() const
 {
-    return 0;
+    return { };
 }
 
 bool FlowLayout::hasHeightForWidth() const
@@ -156,11 +155,11 @@ QSize FlowLayout::sizeHint() const
 QSize FlowLayout::minimumSize() const
 {
     QSize size;
-    QLayoutItem *item;
-    foreach (item, itemList)
+    for (const QLayoutItem *item : std::as_const(itemList))
         size = size.expandedTo(item->minimumSize());
 
-    size += QSize(2*margin(), 2*margin());
+    const QMargins margins = contentsMargins();
+    size += QSize(margins.left() + margins.right(), margins.top() + margins.bottom());
 
     if(!m_prevRect.isEmpty())
     {
@@ -187,17 +186,16 @@ int FlowLayout::doLayout(const QRect &rect, bool testOnly) const
     int lineHeight = 0;
     QSize fixedSize;
     
-    QLayoutItem *item;
     if(m_fixedGrid) {
-      foreach (item, itemList) {
+      for (const QLayoutItem *item : std::as_const(itemList)) {
           fixedSize = fixedSize.expandedTo(item->sizeHint());
       }
     }
 
     QList<QPair<QLayoutItem *, QRect>> line;
 
-    foreach (item, itemList) {
-        QWidget *wid = item->widget();
+    for (QLayoutItem *item : std::as_const(itemList)) {
+        const QWidget *wid = item->widget();
 
         QSize size = m_fixedGrid ? fixedSize : item->sizeHint();
 
@@ -255,7 +253,7 @@ int FlowLayout::smartSpacing(QStyle::PixelMetric pm) const
         return -1;
     } else if (parent->isWidgetType()) {
         QWidget *pw = static_cast<QWidget *>(parent);
-        return pw->style()->pixelMetric(pm, 0, pw);
+        return pw->style()->pixelMetric(pm, nullptr, pw);
     } else {
         return static_cast<QLayout *>(parent)->spacing();
     }
